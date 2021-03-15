c822f6faae58ed9ba139be98f6bfd5c7af26ed64
Demian <dembanakh01@gmail.com>
2021-03-15 18:15:04 +0100

    unit tests fix

--- core/src/test/game/TheMazeGameTest.java
+++ core/src/test/game/TheMazeGameTest.java
@@ -3,9 +3,13 @@ package game;
 import org.junit.Assert;
 import org.junit.Test;
 
-class TheMazeGameTest {
+import static org.junit.Assert.*;
+
+public class TheMazeGameTest {
+
     @Test
-    public void addition_isCorrect() {
+    public void addition() {
         Assert.assertEquals(4, 2 + 2);
     }
+
 }
