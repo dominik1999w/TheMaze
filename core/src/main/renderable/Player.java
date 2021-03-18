e9dac14343ce83b43b65ca9837506b62ef4c60fb
ssalabura <stagerek@gmail.com>
2021-03-18 20:48:26 +0100

    player rotation

--- core/src/main/renderable/Player.java
+++ core/src/main/renderable/Player.java
@@ -1,21 +1,28 @@
 package renderable;
 
 import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.Sprite;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.badlogic.gdx.math.Vector2;
 
 import input.IPlayerInput;
 
 public class Player implements Renderable{
-    private final Texture texture;
+    private final Sprite sprite;
     private final Vector2 position;
+    private float rotation;
     private float speed;
 
     public Player(Vector2 position) {
-        this.texture = new Texture("player.png");
+        sprite = new Sprite(new Texture("player.png"));
+        sprite.setSize(Tile.width, Tile.height);
+        sprite.setOriginCenter();
+
         this.position = position;
         this.position.x *= Tile.width;
         this.position.y *= Tile.height;
+
+        rotation = 0;
         speed = 3;
     }
 
@@ -26,6 +33,12 @@ public class Player implements Renderable{
     public void updatePosition(IPlayerInput playerInput, float deltaTime) {
         position.x += playerInput.getX() * Tile.width * speed * deltaTime;
         position.y += playerInput.getY() * Tile.height * speed * deltaTime;
+        if(playerInput.getX() != 0 || playerInput.getY() != 0) {
+            rotation = (float) (Math.atan2(playerInput.getY(), playerInput.getX()) * (180 / Math.PI));
+        }
+
+        sprite.setPosition(position.x, position.y);
+        sprite.setRotation(rotation);
     }
 
     public void shoot() {
@@ -34,6 +47,6 @@ public class Player implements Renderable{
 
     @Override
     public void render(SpriteBatch spriteBatch) {
-        spriteBatch.draw(texture, position.x, position.y, Tile.width, Tile.height);
+        sprite.draw(spriteBatch);
     }
 }
